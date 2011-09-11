913bdd795d128bef61a5fe05e188ac5e0c0b7e62
keithpitt <me@keithpitt.com>
2011-09-11 21:49:31 +0800

    JSON responses for uploading vendors

--- app/controllers/vendors_controller.rb
+++ app/controllers/vendors_controller.rb
@@ -1,5 +1,7 @@
 class VendorsController < ApplicationController
 
+  respond_to :html, :json
+
   before_filter :find_vendor, :only => [ :show ]
   before_filter :authenticate_user!, :only => [ :new, :create ]
 
@@ -15,10 +17,24 @@ class VendorsController < ApplicationController
     @version = current_user.versions.new(params[:version])
 
     if @version.save
-      flash[:success] = "Vendor uploaded successfully"
-      redirect_to @version.vendor
+      respond_with do |format|
+        format.html {
+          flash[:success] = "Vendor uploaded successfully"
+          redirect_to @version.vendor
+        }
+        format.json {
+          render :json => { :status => "ok", :url => vendor_path(@version.vendor) }
+        }
+      end
     else
-      render :action => :new
+      respond_with do |format|
+        format.html {
+          render :action => :new
+        }
+        format.json {
+          render :json => { :status => "error", :message => @version.errors.full_messages.to_sentence }
+        }
+      end
     end
   end
 
